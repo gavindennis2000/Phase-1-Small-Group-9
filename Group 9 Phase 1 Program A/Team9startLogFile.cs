ca47a819e9c1a14e841e388854502a903a2466bd
gavindennis2000 <135553311+gavindennis2000@users.noreply.github.com>
2024-09-20 14:54:55 -0500

    Created bulk of Part A
    Gavin Dennis
    9/20/24 2:54pm
    Created .cs file that can write to CSV files.

---
+++ Group 9 Phase 1 Program A/Team9startLogFile.cs
@@ -0,0 +1,58 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace CSVLog;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Country { get; set; }
+}
+public class Team9startLogFile
+{
+    static void WriteToCSV(string filename)
+    {
+        try
+        {
+            if (!File.Exists(filename)) 
+            {
+                using (File.Create(filename)) { }
+            }
+
+            List<Person> people = new List<Person>
+            {
+                new Person { Name = "John", Age = 25, Country = "USA" },
+                new Person { Name = "Alice", Age = 30, Country = "Canada" }
+            };
+
+            var configPersons = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+            };
+
+            using (StreamWriter streamWriter = new StreamWriter(filename, true))
+            using (CsvWriter csvWriter = new CsvWriter(streamWriter, configPersons))
+            {
+                csvWriter.WriteRecords(people);
+            }
+            Console.WriteLine("Data written to CSV file.");
+        }
+        catch(Exception ex)
+        {
+            throw;
+        }
+    }
+
+    static void Main()
+    {
+        string fileDirectory = @"C:\Users\Anthony\Documents\GitHub\Phase-1-Small-Group-9\Group 9 Phase 1 Program A\";
+        string fileName = "data.csv";
+        string filePath = Path.Combine(fileDirectory, fileName);
+
+        WriteToCSV(filePath);
+        Console.ReadKey();
+  
+    }
+}
