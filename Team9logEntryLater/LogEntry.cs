eb7a00acb4a186cb1a105d3c67a3d897668e0b5d
Sam Irvin <sbirvin1s@gmail.com>
2024-09-26 23:22:19 -0500

    updateLogs: adds log entry class and note class

---
+++ Team9logEntryLater/LogEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Team9logEntryLater
+{
+	internal class LogEntry
+	{
+		private DateTime? _startDate;
+		private DateTime? _startTime; // Changed from TimeOnly to DateTime?
+		private DateTime? _endDate;
+		private DateTime? _endTime;
+		private int _howManyPeople = 0;
+		private int? _activityCode = null;
+
+		public DateTime? StartDate
+		{
+			get { return _startDate; }
+			set { _startDate = value.HasValue ? value.Value.Date : (DateTime?)null; }
+		}
+		public DateTime? StartTime
+		{
+			get { return _startTime; }
+			set { _startTime = value; }
+		}
+		public DateTime? EndDate
+		{
+			get { return _endDate; }
+			set { _endDate = value.HasValue ? value.Value.Date : (DateTime?)null; }
+		}
+		public DateTime? EndTime
+		{
+			get { return _endTime; }
+			set { _endTime = value; }
+		}
+		public int HowManyPeople
+		{
+			get { return _howManyPeople; }
+			set
+			{
+				if (value > 50 || value < 1) throw new ArgumentOutOfRangeException(nameof(value), "Must have at least one participant and no more than 50");
+			}
+		}
+		public int ActivityCode
+		{
+			// code inspiration taken from https://stackoverflow.com/questions/8860879/in-c-sharp-is-there-any-datatype-to-store-the-hexadecimal-value
+			get { return (int)_activityCode; }
+			set
+			{
+				if (value >= 0x0 && value <= 0xD)
+				{
+					_activityCode = value;
+				}
+				else
+				{
+					throw new ArgumentOutOfRangeException(nameof(value), "Activity Code must be a hexidecimal value between 0 and D");
+				}
+			}
+		}
+		public Note Note { get; set; }
+	}
+}
---
+++ Team9logEntryLater/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team9logEntryLater
+{
+	internal class Note
+	{
+		private bool _isManadatory = false;
+		private string _entry = null;
+
+		public bool IsMandatory
+		{
+			get { return _isManadatory; }
+			set { value = _isManadatory; }
+		}
+
+		public string Entry
+		{
+			get { return _entry; }
+			set
+			{
+				if (value.Length > 80) throw new ArgumentOutOfRangeException(nameof(value), "Note must not be longer than 80 characters");
+				if (value.Contains(",")) throw new ArgumentOutOfRangeException(nameof(value), "Note must not contain commas");
+				if (IsMandatory && string.IsNullOrWhiteSpace(value)) throw new ArgumentOutOfRangeException(nameof(value), "Note cannot be empty when mandatory");
+
+				_entry = value;
+			}
+		}
+	}
+}
